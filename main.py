0d57ccadd69bae7a369bc831985ba003ad51d0de
byungguan-yoon <bkaunyun@gmail.com>
2021-08-19 10:46:44 +0900

    [Create] main api

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return {'data': {'name': 'Jelly'}}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
